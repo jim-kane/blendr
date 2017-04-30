6a90d00c4a219e486c679f565c5781ac8f7b0697
jim.kane@gmail.com <kMCv2$4QdozwAG>
2017-04-30 10:17:44 -0700

    More work on the absolute path class

--- blendr/src/main/java/org/kane/blendr/url/AbsolutePath.java
+++ blendr/src/main/java/org/kane/blendr/url/AbsolutePath.java
@@ -1,7 +1,10 @@
 package org.kane.blendr.url;
 
 import org.jimmutable.core.exceptions.ValidationException;
+import org.jimmutable.core.fields.FieldArrayList;
+import org.jimmutable.core.fields.FieldList;
 import org.jimmutable.core.objects.Stringable;
+import org.jimmutable.core.utils.Optional;
 import org.jimmutable.core.utils.Validator;
 
 /**
@@ -27,6 +30,7 @@ import org.jimmutable.core.utils.Validator;
  */
 public class AbsolutePath extends Stringable
 {
+	transient private FieldList<PathLabel> labels;
 	transient private String extension;
 	
 	public AbsolutePath(String path_str)
@@ -45,28 +49,50 @@ public class AbsolutePath extends Stringable
 		
 		value = value.replace('\\', '/');
 		
-		while( value.contains("//") ) // Remove any double slashses
-			value = value.replace("//", "/");
+		labels = new FieldArrayList();
 		
-		while( value.endsWith("/") )
-			value = value.substring(0, value.length()-1);
+		String label_strs[] = value.split("/");
 		
-		if ( value.length() == 0 ) value = "/";
-		super.setValue(value);
-		
-		// Set the extension...
-		int dot_idx = value.lastIndexOf('.');
-		if ( dot_idx != -1 )
+		for ( String label_str : label_strs )
 		{
-			extension = value.substring(dot_idx+1);
+			if ( label_str.length() == 0 ) continue;
 			
-			if ( extension.contains("/") || extension.length() == 0 )
-				extension = null;
+			try
+			{
+				PathLabel label = new PathLabel(label_str);
+				labels.add(label);
+			}
+			catch(ValidationException e)
+			{
+				super.setValue(null); // Not a valid AbsolutePath because the path contains at least one invalid path label
+				return;
+			}
 		}
 		
+		labels.freeze();
 		
+		// Build the normalized string from the labels..
+		
+		StringBuilder normalized_value = new StringBuilder("/");
+		
+		for ( PathLabel label : labels )
+		{
+			if ( normalized_value.charAt(normalized_value.length()-1) != '/' )
+				normalized_value.append('/');
+			
+			normalized_value.append(label.toString());
+		}
+		
+		super.setValue(normalized_value.toString());
+		
+		// Set the extension
+		extension = null;
+		
+		if ( !labels.isEmpty() ) 
+		{
+			extension = labels.get(labels.size()-1).getOptionalExtension(null);
+		}
 	}
-
 	
 	public void validate() 
 	{
@@ -84,6 +110,46 @@ public class AbsolutePath extends Stringable
 			
 			throw new ValidationException(String.format("Illegal character in path %s, '%c'", getSimpleValue(), ch));
 		}	
-		
+	}
+
+	/**
+	 * Does this path have a valid extension? (e.g. "html", "txt", etc.)
+	 * 
+	 * @return true if the path has a valid extension, false otherwise
+	 */
+	public boolean hasExtension() 
+	{ 
+		return Optional.has(extension, null); 
+	}
+	
+	
+	/**
+	 * Get the extension of the path. Extensions are the string that come
+	 * *after* the last . in the path.
+	 * 
+	 * For example, the extension of /some-directory/foo.txt is "txt" (not *not*
+	 * ".txt"). The path "foo" does not have an extension
+	 * 
+	 * @param default_value
+	 *            The value to return if this path does not have an extension
+	 * 
+	 * @return The extension of the path, or default_value if this path does not
+	 *         have a valid extension
+	 */
+	public String getOptionalExtension(String default_value) 
+	{ 
+		return Optional.getOptional(extension, null, default_value); 
+	}
+	
+	/**
+	 * Get an (frozen, therefore immutable) list of the PathLabel(s) that make up this path
+	 * 
+	 * May be empty
+	 * 
+	 * @return An immutable List of PathLabel(s) that make up this path
+	 */
+	public FieldList<PathLabel> getSimpleLabels() 
+	{ 
+		return labels; 
 	}
 }
